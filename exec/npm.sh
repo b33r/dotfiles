5116faef1720aed172f972d93cb09688436ebbc7
b33r <b33r@users.noreply.github.com>
2015-04-24 13:31:56 +0530

    Npm installers

---
+++ exec/npm.sh
@@ -0,0 +1,13 @@
+# Install node modules globally
+
+## TODO: Check whether node and npm exists
+
+npm install -g bower
+npm install -g express-generator
+npm install -g grunt-cli
+npm install -g gulp
+npm install -g jscs
+npm install -g jshint
+npm install -g karma
+npm install -g mocha
+npm install -g pm2
